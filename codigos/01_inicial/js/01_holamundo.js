c73fca9e48d116961673f41304c33192cfe8d6ac
angelrp2404 <72132869+angelrp2404@users.noreply.github.com>
2021-06-08 10:19:56 +0200

    kj

---
+++ codigos/01_inicial/js/01_holamundo.js
@@ -0,0 +1,52 @@
+//comentario de una lines
+
+/*
+comentario multiple
+
+
+console.log("mensaje de comprobacion pimer proyecto");
+console.log("hola mundo ");
+console.log("esto es otra linea");
+console.log(1, 2, 2, 3, 23, 2, 31);
+console.time("inicio.js");
+//promesas
+console.timeEnd("inicio.js");
+ */
+
+// variables
+// tipo(acceso) nombre = valor
+//constante
+const NOMBRE = "Angel";
+console.log("mi nombre es: " + NOMBRE);
+
+//var --> global, puedo acceder a ella desde cualquier sitio
+var edad = 19;
+console.log(" mi edad es: " + edad);
+
+// let --> variebale que tiene ambito
+let experiencia = true;
+let variableBoolean = true;
+let edadNumber = 20;
+let edadNumberDecimal = 20.4;
+let variableString = "angel";
+let cualquiercosaNull = null;
+let cosasinDefinir = undefined;
+let cosaColeccion = [];
+let cosaColeccionMultiple = [[], [], []];
+let variableObjeto = {
+  nombre: "angel",
+  edad: 20,
+  experiencia: true,
+  hobbies: ["jugar", "trabjar"],
+};
+let fecha = new Date();
+let palabraObject = new String("nombre objeto");
+
+console.log(
+  `Mi nombre es:${NOMBRE} y tengo ${edad} y mi experiencia es ${experiencia}`
+);
+// sacsa el tipo de variable
+    //console.log(typeof edad);
+//isNaN--> comprueba si es uun numero 
+
+
---
+++ codigos/01_inicial/js/02_Operadores.js
@@ -0,0 +1,20 @@
+
+// operador de asignacion 
+
+let numero= 0;
+
+//operadores aritmeticos
+
+let suma = 5+8;
+let resta = 5-8;
+let multi = 5*8;
+let division = 5/8;
+let resto = 5%8;
+
+suma +=4;
+resta -=4;
+multi *=4;
+div +=4;
+suma +=4;
+
+alert(`esto es una alaerta, el resultado es ${suma}`)
